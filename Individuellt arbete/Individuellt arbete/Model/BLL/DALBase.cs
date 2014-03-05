072e77ff9ae762fbefb5d1f5b84e2309a8f29f4e
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-03-05 12:44:24 +0100

    Added the code from AC, will need to implement the stored procedures before I can actually do something though

--- Individuellt arbete/Individuellt arbete/Model/BLL/DALBase.cs
+++ Individuellt arbete/Individuellt arbete/Model/BLL/DALBase.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
-namespace Individuellt_arbete.Model.BLL
+namespace Individuellt_arbete.Model
 {
-    public class DALBase
+    [Serializable]
+    public class ConnectionException : Exception
     {
+        public ConnectionException() { }
+        public ConnectionException(string message) : base(message) { }
+        public ConnectionException(string message, Exception inner) : base(message, inner) { }
+        protected ConnectionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+    abstract public class DALBase
+    {
+        string _connectionString;
+
+        protected SqlConnection CreateConnection()
+        {
+            //Attempts to connect with the QuickOpen function which throws after 5000 ms if we didn't connect
+            //That way we can assume that it's safe to connect to the server and we won't get 30 s of loading
+            SqlExtensions.QuickOpen(new SqlConnection(_connectionString), 5000, "Anslutningen till databasen misslyckades.");
+            return new SqlConnection(_connectionString);
+        }
+        public DALBase()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings["ApplicationService"].ConnectionString;
+        }
     }
 }
