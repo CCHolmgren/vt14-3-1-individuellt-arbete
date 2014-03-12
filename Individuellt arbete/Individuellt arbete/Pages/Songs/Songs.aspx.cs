b72e205ad4ff43ceea2058da595e95bce004a513
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-03-12 11:58:07 +0100

    ErrorChecking and safety

--- Individuellt arbete/Individuellt arbete/Pages/Songs/Songs.aspx.cs
+++ Individuellt arbete/Individuellt arbete/Pages/Songs/Songs.aspx.cs
@@ -27,13 +27,23 @@ namespace Individuellt_arbete.Pages.Songs
         //     string sortByExpression
         public IEnumerable<Individuellt_arbete.Model.Song> SongList_GetData(int maximumRows, int startRowIndex, out int totalRowCount)
         {
-            IEnumerable<Model.Song> songs = Service.getSongList(maximumRows, startRowIndex, out totalRowCount, Convert.ToInt32(Page.RouteData.Values["albumid"]));
-            if(songs.Count()==0)
+            IEnumerable<Model.Song> songs;
+            try 
             {
-                Page.Title = String.Format("Albumet har inga låtar.");
-                ModelState.AddModelError(String.Empty, "Det finns inga låtar associerade med det albumet. Försök igen med ett annat album.");
+                songs = Service.getSongList(maximumRows, startRowIndex, out totalRowCount, Convert.ToInt32(Page.RouteData.Values["albumid"]));
+                if (songs.Count() == 0)
+                {
+                    Page.Title = String.Format("Albumet har inga låtar.");
+                    ModelState.AddModelError(String.Empty, "Det finns inga låtar associerade med det albumet. Försök igen med ett annat album.");
+                }
+                return songs;
             }
-            return songs;
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, ex.Message);
+                totalRowCount = 0;
+                return null;
+            }
         }
     }
 }
